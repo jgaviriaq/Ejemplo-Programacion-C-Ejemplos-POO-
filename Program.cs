f65471710d10a9176603d5e8fe5efdc2c6857656
Julian Gaviria Quintero <jgaviriaq@outlook.com>
2020-09-22 17:25:53 -0500

    Ejercicio1

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Ejercicio1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("CONCESIONARIO");
+
+            Console.WriteLine("Marca");
+            string marca = Console.ReadLine();
+            Console.WriteLine("Serie");
+            string serie = Console.ReadLine();
+            Console.WriteLine("Modelo");
+            short modelo = short.Parse(Console.ReadLine());
+            Console.WriteLine("Placa");
+            string placa = Console.ReadLine();
+
+            // Instanciar la clase vehiculo
+
+            var vehiculoUno = new Vehiculo()
+            {
+              Marca = marca,
+              Serie = serie,
+              Modelo = modelo,
+              Placa = placa  
+
+            };
+
+            Console.WriteLine(vehiculoUno.Placa);
+        }
+    }
+}
